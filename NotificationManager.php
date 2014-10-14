3714ee3d6c17595f6f1dbad92246d216d0d7522d
Dominick Peluso <peluso.dominick@gmail.com>
2014-10-13 20:10:15 -0400

    Finish problem 3

--- NotificationManager.php
+++ NotificationManager.php
@@ -11,15 +11,62 @@ class NotificationManager
 
     public function __construct()
     {
-        // Set the SQS client
-        $this->snsClient = $this->getSqsClient();
+        // Set the SNS client
+        $this->snsClient = $this->getSnsClient();
     }
 
-    // Return the SQS client
-    public function getSqsClient()
+    // Return the SNS client
+    public function getSnsClient()
     {
         $clientParams = array('region' => 'us-east-1');
         $client = SnsClient::factory($clientParams);
         return $client;
     }
+
+    // Create an SNS topic
+    public function createTopic($name)
+    {
+        $response = $this->snsClient->createTopic(array(
+            'Name' => $name
+        ));
+
+        return $response;
+    }
+
+    // Subscribe
+    public function subscribe($arn, $protocol, $endpoint)
+    {
+        $subscribeParams = array(
+            'TopicArn' => $arn,
+            'Protocol' => $protocol,
+            'Endpoint' => $endpoint
+        );
+        $response = $this->snsClient->subscribe($subscribeParams);
+
+        return $response;
+    }
+
+    // Confirm subscription
+    public function confirmSubscription($arn, $token)
+    {
+        $confirmParams = array(
+            'TopicArn' => $arn,
+            'Token' => $token
+        );
+        $response = $this->snsClient->confirmSubscription($confirmParams);
+
+        return $response;
+    }
+
+    // Send a text-based message
+    public function publishPlainTextMessage($arn, $message)
+    {
+        $response = $this->snsClient->publish(array(
+            'TopicArn' => $arn,
+            'Message' => $message
+        ));
+
+        return $response;
+    }
+
 }
--- Problem3App.php
+++ Problem3App.php
@@ -3,3 +3,40 @@
  * CSCI-E90 Assignment 06
  * Dominick Peluso 2014
  */
+
+ini_set('max_execution_time', 300); // Increase max exec time, due to sleep()
+
+include('NotificationManager.php');
+
+$nm = new NotificationManager(); // Get NotificationManager object
+
+// Create an SNS topic
+$topicResponse = $nm->createTopic("MyCoolTopic3");
+$arn = $topicResponse['TopicArn'];
+echo "Topic created. ARN: $arn".'<hr>';
+
+// Subscribe a couple of people via email
+$subscribe1 = $nm->subscribe($arn, 'email', 'peluso.dominick@gmail.com');
+$subscribe2 = $nm->subscribe($arn, 'email', 'dpeluso@g.harvard.edu');
+
+// Get the request IDs
+$requestId1 = $subscribe1['ResponseMetadata']['RequestId'];
+$requestId2 = $subscribe2['ResponseMetadata']['RequestId'];
+
+// Print subscription request ID
+echo "Subscribed user. Request ID: $requestId1".'<hr>';
+echo "Subscribed user. Request ID: $requestId2".'<hr>';
+
+// Confirm subscriptions programmatically
+    # Wasn't able to get this working, don't want to deal with parsing emails...
+#$confirmResponse1 = $nm->confirmSubscription($arn, $requestId1);
+#$confirmResponse2 = $nm->confirmSubscription($arn, $requestId2);
+
+// Sleep while I wait to confirm subscriptions
+sleep(30);
+
+// Publish a message
+$message = 'Example message!!!!';
+$publishResponse = $nm->publishPlainTextMessage($arn, $message);
+$messageId = $publishResponse['MessageId'];
+echo "Message published. ID: $messageId".'<hr>';
